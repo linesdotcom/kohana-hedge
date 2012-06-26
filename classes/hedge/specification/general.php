c5bdb536f58a7fe5448be2fce4a8c7c1f4f705ca
Miodrag Tokić <mtokic@gmail.com>
2012-06-26 11:40:10 +0200

    Added general specification

---
+++ classes/hedge/specification/general.php
@@ -0,0 +1,59 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * General specification class
+ *
+ * @package    Hedge
+ * @category   Specification
+ * @author     Miodrag Tokić
+ * @copyright  (c) 2012, Miodrag Tokić
+ * @license    MIT
+ */
+abstract class Specification_General {
+
+	/**
+	 * @var  Validation
+	 */
+	protected $_validation;
+
+	/**
+	 * @var  string  Error messages file
+	 */
+	protected $_error_file = 'validation';
+
+	/**
+	 * Sets Validation object if provided
+	 *
+	 * @param   Validation
+	 * @return  void
+	 */
+	public function __construct(Validation $validation = NULL)
+	{
+		$this->_validation = $validation;
+	}
+
+	/**
+	 * Validation object setter/getter
+	 *
+	 * @param   Validation
+	 * @return  Validation
+	 */
+	public function validation(Validation $validation = NULL)
+	{
+		if ($validation !== NULL)
+		{
+			$this->_validation = $validation;
+		}
+
+		return $this->_validation;
+	}
+
+	/**
+	 * Returns errors (broken rules)
+	 *
+	 * @return  array
+	 */
+	public function errors()
+	{
+		return $this->_validation->errors($this->_error_file);
+	}
+}
---
+++ classes/specification/general.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Specification_General extends Hedge_Specification_General {}
